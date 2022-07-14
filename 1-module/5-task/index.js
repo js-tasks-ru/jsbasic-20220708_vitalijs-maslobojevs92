a41645634a54a631ec1b031c278a269036cff6f0
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-14 15:41:55 +0300

    solved 1-module 5-task truncedStr

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let truncedStr = str.slice(0, maxlength - 1);
+
+    return truncedStr + "…";
+  }
+
+  return str;
 }
+
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
