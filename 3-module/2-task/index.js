3dab47027e469ce29333117121d72ae21f0f2dfe
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-19 20:37:41 +0300

    solved 3-nodule 2-task filterRange

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArr = arr.slice(0);
+
+  return newArr.filter((element) => {
+    for (let i = a; i <= b; i++) {
+      if (i === element) return true;
+    }
+  });
 }
