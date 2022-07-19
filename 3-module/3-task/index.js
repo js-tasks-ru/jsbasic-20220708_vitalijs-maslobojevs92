9511e57f3305ca07e3f04568488796ff059576b1
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-19 21:21:48 +0300

    solved 3-module 3-task camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("").map((element, index, arr) => {
+    if (element === "-") return;
+    if (arr[index - 1] === "-") return element.toUpperCase();
+
+    return element;
+  });
+
+  return arr.join("");
 }
+
+console.log(camelize("-webkit-transition"));
