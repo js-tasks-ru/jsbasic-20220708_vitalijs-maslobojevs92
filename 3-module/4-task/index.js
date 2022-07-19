7fab9a8745cec113fc66eeb87aa58dfb7fa9a0e1
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-19 21:49:06 +0300

    solved 3-module 4-task salary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let arr = users
+    .filter((user) => {
+      if (user.age <= age) return user;
+    })
+    .map((user, index, arr) => {
+      return index === arr.length - 1
+        ? `${user.name}, ${user.balance}`
+        : `${user.name}, ${user.balance}\n`;
+    });
+
+  return arr.join("");
 }
