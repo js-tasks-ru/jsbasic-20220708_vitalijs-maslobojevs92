27538bb92654515522be3427bb466a4a2b95e028
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-24 17:43:53 +0300

    solved 4-module 2-task makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.querySelectorAll("TR"); //table.firstElementChild.children;
+  let i = 0;
+
+  for (let row of rows) {
+    let cells = row.children;
+
+    cells[i].style.backgroundColor = "red";
+    i++;
+
+    continue;
+  }
 }
