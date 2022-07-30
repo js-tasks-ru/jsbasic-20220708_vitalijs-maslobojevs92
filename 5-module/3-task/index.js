b1b040bd0793d344e8b0b708aeec7c2dda8d8c24
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-07-31 00:26:24 +0300

    changed varriable name in 5-module 3-task carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,9 +2,9 @@ function initCarousel() {
   let carousel = document.querySelector(".carousel");
   let leftArrowBtn = carousel.querySelector(".carousel__arrow_left");
   let rightArrowBtn = carousel.querySelector(".carousel__arrow_right");
-  let slides = carousel.querySelector(".carousel__inner");
+  let slidesContainer = carousel.querySelector(".carousel__inner");
   let slideWidth = 0;
-  let slidesCount = 0;
+  let slidePos = 0;
 
   changeButtonState();
 
@@ -25,20 +25,20 @@ function initCarousel() {
 
   function moveSlide({ direction }) {
     if (direction === "right") {
-      slideWidth = slideWidth - slides.offsetWidth;
-      slides.style.transform = `translateX(${slideWidth}px)`;
-      slidesCount++;
+      slideWidth = slideWidth - slidesContainer.offsetWidth;
+      slidesContainer.style.transform = `translateX(${slideWidth}px)`;
+      slidePos++;
     } else {
-      slideWidth = slideWidth + slides.offsetWidth;
-      slides.style.transform = `translateX(${slideWidth}px)`;
-      slidesCount--;
+      slideWidth = slideWidth + slidesContainer.offsetWidth;
+      slidesContainer.style.transform = `translateX(${slideWidth}px)`;
+      slidePos--;
     }
   }
 
   function changeButtonState() {
     rightArrowBtn.style.display =
-      slidesCount == slides.childElementCount - 1 ? "none" : "";
+      slidePos == slidesContainer.childElementCount - 1 ? "none" : "";
 
-    leftArrowBtn.style.display = slidesCount == 0 ? "none" : "";
+    leftArrowBtn.style.display = slidePos == 0 ? "none" : "";
   }
 }
